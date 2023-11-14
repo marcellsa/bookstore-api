6e52499c21387871fac106c0e6b7ed8bfdc4c095
Marcel Albuquerque <msalbuquerque@hotmail.com>
2023-11-14 16:40:51 -0300

    feat: conectando Mongo Atlas com API

--- src/app.js
+++ src/app.js
@@ -1,5 +1,16 @@
 import express from 'express';
+import connectToDatabase from './config/database.js';
 import routes from './routes/index.js';
+
+const connection = await connectToDatabase();
+
+connection.on('error', (erro) => {
+  console.error('Erro na conexão com o banco de dados:', erro);
+});
+
+connection.once('open', () => {
+  console.log('Conexão bem-sucedida! Banco de dados pronto para uso.');
+});
 
 const app = express();
 routes(app);
--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const connectToDatabase = async () => {
-  await mongoose.connect(process.env.DB_CONNECTION_STRING);
+  mongoose.connect(process.env.DB_CONNECTION_STRING);
   return mongoose.connection;
 };
 
