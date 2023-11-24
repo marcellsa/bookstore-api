315bfb816c75c16f7d4de25b38d1a32cdcfd825e
Marcel Albuquerque <msalbuquerque@hotmail.com>
2023-11-24 17:20:11 -0300

    test(contr): implementando testes saveBook

--- tests/controllers/bookController.test.js
+++ tests/controllers/bookController.test.js
@@ -10,4 +10,23 @@ describe('BookController', () => {
 
     expect(response.data).toHaveLength(4);
   });
+
+  it('should save a book', async () => {
+    const data = {
+      titulo: 'Título Test1 1',
+      editora: 'Editora Teste 1',
+      preco: 99.99,
+      paginas: 100,
+    };
+
+    const response = await axios({
+      url: 'http://localhost:3000/livros',
+      method: 'post',
+      data,
+    });
+
+    const bookTest = response.data;
+
+    expect(bookTest.titulo).toBe(data.titulo);
+  });
 });
