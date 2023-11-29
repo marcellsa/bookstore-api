9a7ac4ab30b1ebf63dbbbc3bd1f5d8a31a9ef2a7
Marcel Albuquerque <msalbuquerque@hotmail.com>
2023-11-29 12:12:43 -0300

    test(contr): implementando testes do método updateBook

--- tests/controllers/bookController.test.js
+++ tests/controllers/bookController.test.js
@@ -7,7 +7,7 @@ const URL = 'http://localhost:3000';
 const request = (url, method, data) => axios({ url, method, data });
 
 describe('BookController', () => {
-  it('should get a list of books', async () => {
+  it.skip('should get a list of books', async () => {
     const bookData1 = {
       titulo: 'Título Test',
       editora: 'Editora Teste',
@@ -24,12 +24,12 @@ describe('BookController', () => {
     await request(`${URL}/livros/${testBook.data.book._id}`, 'delete');
   });
 
-  it('should save a book', async () => {
+  it.skip('should save a book', async () => {
     const bookData2 = {
       titulo: 'Título Test 2',
       editora: 'Editora Teste 2',
       preco: 89.99,
-      paginas: 200,
+      paginas: 150,
     };
 
     const response = await request(`${URL}/livros`, 'post', bookData2);
@@ -37,5 +37,30 @@ describe('BookController', () => {
     const bookTest = response.data.book;
 
     expect(bookTest.titulo).toEqual(bookData2.titulo);
+
+    await request(`${URL}/livros/${bookTest._id}`, 'delete');
+  });
+
+  it('should updtae a book', async () => {
+    const bookData3 = {
+      titulo: 'Título Test 3',
+      editora: 'Editora Teste 3',
+      preco: 79.99,
+      paginas: 250,
+    };
+
+    const newBook = await request(`${URL}/livros`, 'post', bookData3);
+
+    const savedBook = newBook.data.book;
+
+    const updatedBook = {
+      paginas: 200,
+    };
+
+    const response = await request(`${URL}/livros/${savedBook._id}`, 'put', updatedBook);
+
+    expect(response.data).toEqual({ message: 'Livro atualizado' });
+
+    await request(`${URL}/livros/${savedBook._id}`, 'delete');
   });
 });
