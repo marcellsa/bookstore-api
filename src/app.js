6c6291c8315886cf52eafb22f344390d5c03a3e7
Marcel Albuquerque <msalbuquerque@hotmail.com>
2024-03-12 17:39:05 -0300

    chore: Configurar e conectar MongoDB Atlas e API

--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
 import express from "express";
+import connectToDatabase from "./config/database.js";
+
+const connection = await connectToDatabase();
+
+connection.on("error", (erro) => {
+  console.error("Erro na conexão com o banco de dados:", erro);
+});
+
+connection.once("open", () => {
+  console.log("Conexão bem-sucedida! Banco de dados pronto para uso.");
+});
 
 const app = express();
 
