a1d80d1f8db601470673a9447def99c28ed18ba6
Marcel Albuquerque <msalbuquerque@hotmail.com>
2024-03-14 09:12:39 -0300

    feat: Implementar método GET para listar todos os livros

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -10,6 +10,15 @@ class BookController {
     }
   }
 
+  static async getBooks(req, res) {
+    try {
+      const listBooks = await book.find({});
+      res.status(200).json(listBooks);
+    } catch (erro) {
+      res.status(500).json({ message: `${erro.message} - falha na requisição` });
+    }
+  }
+
   
 }
 
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -4,6 +4,7 @@ import BookController from "../controllers/bookController.js";
 const routes = express.Router();
 
 routes
-  .post("/livros", BookController.createBook);
+  .post("/livros", BookController.createBook)
+  .get("/livros", BookController.getBooks);
 
 export default routes;
