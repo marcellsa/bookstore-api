5421eaac23b5ac418d589928eb13ee4c3e89553d
Marcel Albuquerque <msalbuquerque@hotmail.com>
2024-03-16 15:25:25 -0300

    refactor: Adicionar chamada next(erro) no authorController

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -2,12 +2,12 @@ import { author } from "../models/Author.js";
 
 class AuthorController {
 
-  static async createAuthor (req, res) {
+  static async createAuthor (req, res, next) {
     try {
       const newAuthor = await author.create(req.body);
       res.status(201).json({ message: "criado com sucesso", autor: newAuthor });
     } catch (erro) {
-      res.status(500).json({ message: `${erro.message} - falha ao cadastrar autor` });
+      next(erro);
     }
   }
 
@@ -20,33 +20,45 @@ class AuthorController {
     }
   }
 
-  static async getAuthorById (req, res) {
+  static async getAuthorById (req, res, next) {
     try {
       const id = req.params.id;
       const authorFound = await author.findById(id);
-      res.status(200).json(authorFound);
+      if (authorFound != null){
+        res.status(200).json(authorFound);
+      } else {
+        res.status(404).send({message: "Id do Autor não localizado."});
+      }
     } catch (erro) {
-      res.status(500).json({ message: `${erro.message} - falha na requisição do autor` });
+      next(erro);
     }
   }  
 
-  static async updateAuthor (req, res) {
+  static async updateAuthor (req, res, next) {
     try {
       const id = req.params.id;
-      await author.findByIdAndUpdate(id, req.body);
-      res.status(200).json({ message: "autor atualizado" });
+      const authorFound = await author.findByIdAndUpdate(id, {$set: req.body});
+      if (authorFound !== null){
+        res.status(200).json({ message: "autor atualizado" });
+      } else {
+        res.status(404).send({message: "Id do Autor não localizado."});
+      }
     } catch (erro) {
-      res.status(500).json({ message: `${erro.message} - falha na atualização` });
+      next(erro);
     }
   }
 
-  static async deleteAuthor (req, res) {
+  static async deleteAuthor (req, res, next) {
     try {
       const id = req.params.id;
-      await author.findByIdAndDelete(id);
-      res.status(200).json({ message: "autor excluído com sucesso" });
+      const authorFound = await author.findByIdAndDelete(id);
+      if (authorFound !== null){
+        res.status(200).json({ message: "autor excluído com sucesso" });
+      } else {
+        res.status(404).send({message: "Id do Autor não localizado."});
+      }
     } catch (erro) {
-      res.status(500).json({ message: `${erro.message} - falha na exclusão` });
+      next(erro);
     }
   }
 }
