c7fd78b62fa2c7d33b71bdca09f276021645999e
Marcel Albuquerque <msalbuquerque@hotmail.com>
2024-03-17 10:37:47 -0300

    refactor: Adicionar authorSchema no modelo de livro

--- src/models/Book.js
+++ src/models/Book.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { authorSchema } from "./Author";
 
 const bookSchema = new mongoose.Schema({
   id: { type: mongoose.Schema.Types.ObjectId },
@@ -6,7 +7,7 @@ const bookSchema = new mongoose.Schema({
   editora: { type: String },
   preco: { type: Number },
   paginas: { type: Number },
-  autor: { type: String },
+  autor: authorSchema,
 }, { versionKey: false });
 
 const book = mongoose.model("livros", bookSchema);
