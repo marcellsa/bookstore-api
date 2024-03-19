e1a7f14ed3c10ee57c90a68c95b27b7524e51085
Marcel Albuquerque <msalbuquerque@hotmail.com>
2024-03-19 17:07:20 -0300

    refactor: Adicionar validações de campos obrigatórios no modelo de livros

--- src/models/Book.js
+++ src/models/Book.js
@@ -3,10 +3,10 @@ import { authorSchema } from "./Author.js";
 
 const bookSchema = new mongoose.Schema({
   id: { type: mongoose.Schema.Types.ObjectId },
-  titulo: { type: String, required: true },
-  editora: { type: String },
+  titulo: { type: String, required: [true, "O título do livro é obrigatório"] },
+  editora: { type: String, required: [true, "A editora é obrigatória"] },
   preco: { type: Number },
-  paginas: { type: Number },
+  paginas: { type: Number, min: 10, max: 5000 },
   autor: authorSchema
 }, { versionKey: false });
 
