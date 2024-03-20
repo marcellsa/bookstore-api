cfc5aeda0f059ede1c8c186b0dd650c6de703caa
Marcel Albuquerque <msalbuquerque@hotmail.com>
2024-03-20 16:51:57 -0300

    refactor: Adicionar validação personalizada no campo paginas do modelo de livros

--- src/models/Book.js
+++ src/models/Book.js
@@ -6,7 +6,14 @@ const bookSchema = new mongoose.Schema({
   titulo: { type: String, required: [true, "O título do livro é obrigatório"] },
   editora: { type: String, required: [true, "A editora é obrigatória"] },
   preco: { type: Number },
-  paginas: { type: Number, min: 10, max: 5000 },
+  paginas: { type: Number, 
+    validate: {
+      validator: (valor) => {
+        return valor >= 10 && valor <= 5000;
+      },
+      message: "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"
+    }
+  },
   autor: authorSchema
 }, { versionKey: false });
 
