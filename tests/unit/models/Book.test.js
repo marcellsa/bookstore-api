e2ac76c79cce55ed4466da659c10cc091a7251d6
Marcel Albuquerque <msalbuquerque@hotmail.com>
2024-03-29 14:51:34 -0300

    test: Adicionar testes do modelo de Livro

---
+++ tests/unit/models/Book.test.js
@@ -0,0 +1,7 @@
+import Book from "../../../src/models/Book.js";
+
+describe("Testes do Modelo Book", () => {
+  it("deve criar um livro com todas as informações necessárias", async () => {
+    // Teste aqui...
+  });
+});
